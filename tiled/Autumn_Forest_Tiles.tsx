5b649bb37bfceae4e029ed9f60b0a830d49dd218
Kevin Holland <kevinholland94@gmail.com>
2025-11-25 16:05:11 -0500

    New tiled stuff

---
+++ tiled/Autumn_Forest_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Autumn_Forest_Tiles" tilewidth="16" tileheight="16" tilecount="800" columns="40">
+ <image source="Autumn_Forest_Tiles.png" width="640" height="320"/>
+</tileset>
