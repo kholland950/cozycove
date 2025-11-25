1fe1dc8e4e685830ddf4994917bd5dda0166ac43
Kevin Holland <kevinholland94@gmail.com>
2025-11-25 15:38:33 -0500

    delete og tiled stuff

--- static/assets/Autumn_Forest_Tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Autumn_Forest_Tiles" tilewidth="16" tileheight="16" tilecount="800" columns="40">
- <image source="../../../../Downloads/Autumn Forest/Autumn_Forest_Tiles.png" width="640" height="320"/>
-</tileset>
