fd3148136bd4b4da0aef8314751764f29aa486e2
Kevin Holland <kevinholland94@gmail.com>
2025-11-25 22:14:12 -0500

    Tester tilemap (#4)

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,7 +2,6 @@ import { AUTO, Game } from 'phaser'
 import { Boot } from './scenes/Boot'
 import { Game as MainGame } from './scenes/Game'
 import { GameOver } from './scenes/GameOver'
-import { MainMenu } from './scenes/MainMenu'
 import { Preloader } from './scenes/Preloader'
 
 //  Find out more information about the Game Config at:
@@ -13,7 +12,7 @@ const config: Phaser.Types.Core.GameConfig = {
   height: 768,
   parent: 'game-container',
   backgroundColor: '#028af8',
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  scene: [Boot, Preloader, MainGame, GameOver],
 }
 
 const StartGame = (parent: string) => {
